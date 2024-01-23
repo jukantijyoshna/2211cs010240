bdf66bfa93d2a7effca61053e9e80c72ebc4363e
jukantijyoshna <157376029+jukantijyoshna@users.noreply.github.com>
2024-01-23 14:18:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Helllo world</h1>
+    
+      <p><span class="span">Name: </span>Jyoshna</p>
+      <p><span class="span">Roll no: </span>2211cs010240</p>
+      <p><span class="span">CGPA: </span>9.44</p>
+    </div>
+  );
+}
+
+export default App;
